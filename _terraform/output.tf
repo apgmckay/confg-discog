4dd75cc0f9c63b3af8bba39bf79f86b4794b7d5d
apgmckay <apgmckay@gmail.com>
2023-06-02 20:21:25 +0100

    update output

---
+++ _terraform/output.tf
@@ -0,0 +1,9 @@
+output "someconfig_url" {
+  value     = aws_ssm_parameter.someconfig_url
+  sensitive = true
+}
+
+output "someconfig_user" {
+  value     = aws_ssm_parameter.someconfig_user
+  sensitive = true
+}
