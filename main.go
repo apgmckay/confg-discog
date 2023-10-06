46e40a986f9b1aa3ba78ba5c715e97051b1a9d6c
apgmckay <apgmckay@gmail.com>
2023-10-06 14:59:48 +0100

    small clean up of code, less logging and move some config into main function

--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	confg_discog "confg-discog/pkg"
-	"log"
 )
 
 var interval = 2
 var confdLogLevel = "info"
 var confdBackend = "ssm"
+var confdConfigFile = "/etc/confd/conf.d/myconfig.toml"
 
 func main() {
-	log.Println("entrypoint: Hello!")
 	cd := confg_discog.New(interval)
+
+	cd.SetConfigFile(confdConfigFile)
 	cd.SetBackend(confdBackend)
+	cd.SetLogLevel(confdLogLevel)
+
 	cd.Run()
 }
--- pkg/confg_discog.go
+++ pkg/confg_discog.go
@@ -1,7 +1,6 @@
 package confg_discog
 
 import (
-	"log"
 	"os"
 	"os/exec"
 	"time"
@@ -16,8 +15,7 @@ type ConfgDiscog struct {
 }
 
 func New(runInterval int) ConfgDiscog {
-	logPrefix := "entrypoint:"
-	log.Printf("%s new ConfgDiscog!", logPrefix)
+	logPrefix := "confg_discog:"
 	return ConfgDiscog{
 		interval:  runInterval,
 		logPrefix: logPrefix,
@@ -25,55 +23,38 @@ func New(runInterval int) ConfgDiscog {
 }
 
 func (cd *ConfgDiscog) SetInterval(runInterval int) {
-	log.Printf("%s set interval == %d\n", cd.logPrefix, runInterval)
 	cd.interval = runInterval
 }
 
 func (cd *ConfgDiscog) SetBackend(backend string) {
-	log.Printf("%s set backend == %s\n", cd.logPrefix, backend)
 	cd.backend = backend
 }
 
 func (cd *ConfgDiscog) SetLogLevel(logLevel string) {
-	log.Printf("%s set backend == %s\n", cd.logPrefix, logLevel)
 	cd.logLevel = logLevel
 }
 
 func (cd *ConfgDiscog) SetConfigFile(configFileName string) {
-	log.Printf("entrypoint: set config-file == %s\n", configFileName)
 	cd.configFile = configFileName
 }
 
 func (cd *ConfgDiscog) Run() {
-	log.Printf("%s ConfgDiscog Running!", cd.logPrefix)
-
-	confdLogLevel := "info"
-
-	log.Printf("%s loglevel set to %s\n", cd.logPrefix, confdLogLevel)
-
 	ticker := time.NewTicker(time.Duration(cd.interval) * time.Second)
 	defer ticker.Stop()
 
-	cd.SetConfigFile("/etc/confd/conf.d/myconfig.toml")
-	cd.runConfdOnetime()
-
-	cd.SetConfigFile("")
 	for range ticker.C {
-		cd.runConfdOnetime()
+		cd.RunConfdOnetime()
 	}
 }
 
-func (cd *ConfgDiscog) runConfdOnetime() error {
+func (cd *ConfgDiscog) RunConfdOnetime() error {
 	var configFileArg []string
 	if len(cd.configFile) > 0 {
 		configFileArg = []string{"-config-file", cd.configFile}
 	}
 	confidCommand := []string{"confd", "-onetime", "-backend", cd.backend, "-log-level", cd.logLevel}
 
-	log.Printf("%s starting command: %v", cd.logPrefix, confidCommand)
-
 	confidCommand = append(confidCommand, configFileArg...)
-	log.Printf("%s starting command: %v", cd.logPrefix, confidCommand)
 
 	err := runCommand(confidCommand[0], confidCommand[1:])
 	if err != nil {
