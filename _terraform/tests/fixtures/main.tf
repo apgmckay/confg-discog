32b1433fd36444bf7b09bc1ac943923679603b79
apgmckay <apgmckay@gmail.com>
2023-10-10 11:09:49 +0100

    move module into root of module

---
+++ _terraform/tests/fixtures/main.tf
@@ -0,0 +1,38 @@
+# TODO: this should be moved into tests
+module "app" {
+  source = "../../"
+
+  # TODO: this should be a list
+  platform_params_path_prefix = "/test/"
+  parameter_prefix            = "app"
+  params = [{
+    name  = "app_username"
+    value = "andy"
+    type  = "String"
+    },
+    {
+      name  = "app_password"
+      value = "mysafepassword"
+      type  = "SecureString"
+    },
+    {
+      name  = "app_url"
+      value = "myapiurl.co.uk"
+      type  = "String"
+    },
+    {
+      name  = "api_token"
+      value = "6f5902ac237024bdd0c176cb93063dc4"
+      type  = "SecureString"
+    }
+  ]
+
+  # TODO: this will need delting
+  template_render = false
+
+  confg_discog_template_output_path = "../../confd/conf.d/"
+  confg_discog_config_file          = "myconfig.toml"
+  confg_discog_app_config_file      = "myconfig.sh"
+  confg_discog_src_file             = "myconfig.sh.tmpl"
+  confg_discog_dest_file            = "/tmp/myconfig.sh"
+}
---
+++ _terraform/tests/tf_confg_discog_test.go
@@ -0,0 +1,21 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestTerraformHelloWorldExample(t *testing.T) {
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "./fixtures",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the values of output variables and check they have the expected values.
+	//	output := terraform.Output(t, terraformOptions, "hello_world")
+	//	assert.Equal(t, "Hello, World!", output)
+}
